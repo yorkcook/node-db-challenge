f989ea8f00b703e2e87309c0cd39ddaa629bcba0
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-16 14:32:23 -0400

    WIP: completed is now showing as true or false

--- project-model.js
+++ project-model.js
@@ -1,5 +1,7 @@
 module.exports = {
-  findResources
+  findResources,
+  findTasks,
+  getProjects
 };
 
 const express = require("express");
@@ -10,10 +12,25 @@ const knexConfig = require("./knexfile.js");
 
 const db = knex(knexConfig.development);
 
+function getProjects() {
+  return db("projects");
+}
+
 function findResources() {
   return db("projects as p")
     .innerJoin("resources as r", "p.id", "r.id")
     .select("r.resource_name");
+}
+
+function findTasks() {
+  return db("projects as p")
+    .innerJoin("tasks as t", "p.id", "t.id")
+    .select(
+      "t.task_description",
+      "t.project_completed",
+      "p.project_name",
+      "p.project_description"
+    );
 }
 
 //  adding tasks.
--- project-router.js
+++ project-router.js
@@ -10,31 +10,41 @@ const db = knex(knexConfig.development);
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  db("projects")
-    .then(projects => {
-      res.status(200).json(projects);
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "No, no, no you don't get to see dem projects" });
+router.get("/", async (req, res) => {
+  try {
+    const projects = await Projects.getProjects();
+    projects.map(projects => {
+      projects.project_completed == 0
+        ? (projects.project_completed = "false")
+        : (projects.project_completed = "true");
     });
+    if (projects) {
+      res.json(projects);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find steps for given scheme" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get steps" });
+  }
 });
 
-router.get("/tasks", (req, res) => {
-  const task = req.params.id;
-  db("projects")
-    .findTasks(task)
-    .then(tasks => {
-      res.status(200).json(tasks);
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "No, no, no you don't get to see dem projects" });
-    });
-});
+// router.get("/", (req, res) => {
+//   db("projects")
+//     .then(projects => {
+//         projects.map(projects => {
+//       projects.project_completed == 0
+//         ? (projects.project_completed = "false")
+//         : (projects.project_completed = "true");
+//       res.status(200).json(projects);
+//     })
+//     .catch(error => {
+//       res
+//         .status(500)
+//         .json({ message: "No, no, no you don't get to see dem projects" });
+//     });
+// });
 
 router.post("/", (req, res) => {
   const project = req.body;
@@ -48,6 +58,22 @@ router.post("/", (req, res) => {
         .status(500)
         .json({ message: "Nobody wants that kind of stinking project." });
     });
+});
+
+router.get("/resources", async (req, res) => {
+  try {
+    const resources = await Projects.findResources();
+
+    if (resources.length) {
+      res.json(resources);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find steps for given scheme" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get steps" });
+  }
 });
 
 router.post("/resources", (req, res) => {
@@ -78,14 +104,30 @@ router.post("/tasks", (req, res) => {
     });
 });
 
-router.get("/resources", async (req, res) => {
-  //const { id } = req.params;
+// router.get("/tasks", (req, res) => {
+//   const task = req.params.id;
+//   db("tasks")
+//     //Projects.findTasks()
+//     .then(tasks => {
+//       res.status(200).json(tasks);
+//     })
+//     .catch(error => {
+//       res
+//         .status(500)
+//         .json({ message: "No, no, no you don't get to see dem projects" });
+//     });
+// });
 
+router.get("/tasks", async (req, res) => {
   try {
-    const resources = await Projects.findResources();
-
-    if (resources.length) {
-      res.json(resources);
+    const task = await Projects.findTasks();
+    task.map(task => {
+      task.project_completed == 0
+        ? (task.project_completed = "false")
+        : (task.project_completed = "true");
+    });
+    if (task) {
+      res.json(task);
     } else {
       res
         .status(404)
@@ -95,23 +137,5 @@ router.get("/resources", async (req, res) => {
     res.status(500).json({ message: "Failed to get steps" });
   }
 });
-
-// router.get("/tasks", async (req, res) => {
-//   //const { id } = req.params;
-
-//   try {
-//     const tasks = await Projects;
-
-//     if (tasks) {
-//       res.json(tasks);
-//     } else {
-//       res
-//         .status(404)
-//         .json({ message: "Could not find steps for given scheme" });
-//     }
-//   } catch (err) {
-//     res.status(500).json({ message: "Failed to get steps" });
-//   }
-// });
 
 module.exports = router;
