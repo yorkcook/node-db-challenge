289e5f8058c1d5ebea825064ad9792d88fc61037
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-16 13:55:19 -0400

    WIP: everything working except get and post for tasks

--- project-model.js
+++ project-model.js
@@ -1,4 +1,6 @@
-module.exports = {};
+module.exports = {
+  findResources
+};
 
 const express = require("express");
 
@@ -7,3 +9,13 @@ const knex = require("knex");
 const knexConfig = require("./knexfile.js");
 
 const db = knex(knexConfig.development);
+
+function findResources() {
+  return db("projects as p")
+    .innerJoin("resources as r", "p.id", "r.id")
+    .select("r.resource_name");
+}
+
+//  adding tasks.
+//  retrieving a list of tasks. The list of tasks should include the project name and project description.
+//  When returning project or task information, the completed property should be true or false.
--- project-router.js
+++ project-router.js
@@ -2,6 +2,116 @@ const express = require("express");
 
 const Projects = require("./project-model.js");
 
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
+
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  db("projects")
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "No, no, no you don't get to see dem projects" });
+    });
+});
+
+router.get("/tasks", (req, res) => {
+  const task = req.params.id;
+  db("projects")
+    .findTasks(task)
+    .then(tasks => {
+      res.status(200).json(tasks);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "No, no, no you don't get to see dem projects" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+  db("projects")
+    .insert(project, "id")
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Nobody wants that kind of stinking project." });
+    });
+});
+
+router.post("/resources", (req, res) => {
+  const resource = req.body;
+  db("resources")
+    .insert(resource, "id")
+    .then(resource => {
+      res.status(200).json(resource);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Nobody wants that kind of stinking resource." });
+    });
+});
+
+router.post("/tasks", (req, res) => {
+  const task = req.body;
+  db("tasks")
+    .insert(task, "id")
+    .then(task => {
+      res.status(200).json(task);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Nobody wants that kind of stinking task." });
+    });
+});
+
+router.get("/resources", async (req, res) => {
+  //const { id } = req.params;
+
+  try {
+    const resources = await Projects.findResources();
+
+    if (resources.length) {
+      res.json(resources);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find steps for given scheme" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get steps" });
+  }
+});
+
+// router.get("/tasks", async (req, res) => {
+//   //const { id } = req.params;
+
+//   try {
+//     const tasks = await Projects;
+
+//     if (tasks) {
+//       res.json(tasks);
+//     } else {
+//       res
+//         .status(404)
+//         .json({ message: "Could not find steps for given scheme" });
+//     }
+//   } catch (err) {
+//     res.status(500).json({ message: "Failed to get steps" });
+//   }
+// });
 
 module.exports = router;
